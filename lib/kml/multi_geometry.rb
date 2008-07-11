0d2a65883d6b276a7af9221f86ae2a41cdcff304
andykram <andrew.kramolisch.07@cnu.edu>
2008-07-11 13:08:42 -0400

    MultiGeometry now inherits from Geometry (as I should have done in the first place) and implements a render method that hopefully works Like it fucking should have in the beginning Coming soon to a kml library near you: KML::Primitives ... or at least i fucking hope so ...

--- lib/kml/multi_geometry.rb
+++ lib/kml/multi_geometry.rb
@@ -1,5 +1,7 @@
 module KML
-  class MultiGeometry < Geometry
-    
+  class MultiGeometry < Geometry 
+    def render(xm=Builder::XmlMarkup.new(:indent => 2))
+      xm.MultiGeometry { features.each { |f| f.render(xm) } }
+    end    
   end
 end
