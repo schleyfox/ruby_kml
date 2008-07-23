7ab99c4bb3a0569b192c479c5aece6be476b6e7b
andykram <andrew.kramolisch.07@cnu.edu>
2008-07-23 16:34:56 -0400

    Model class bitches

--- lib/kml/model.rb
+++ lib/kml/model.rb
@@ -1,4 +1,40 @@
 module KML
   class Model < Geometry
+
+    def initialize(params={})
+      @lng, @lat, @alt = *params[:location]
+      @heading, @tilt, @roll = *params[:orientation] || [0, 0, 0]
+      @x, @y, @z = *params[:scale] || [1, 1, 1]
+      @link = params[:link]
+      @id = params[:id] if params[:id]
+    end
+
+    def id
+      @id || "model_#{@link.href}_#{@x}_#{@y}"
+    end
+
+    def render(xm = Builder::XmlMarkup.new(:indent => 2))
+      xm.Model :id => id do
+        xm.altitudeMode(altitude_mode) if altitude_mode_set?
+        xm.Location do
+          xm.longitude @lng
+          xm.latitude  @lat
+          xm.altitude  @alt
+        end
+        xm.Orientation do
+          xm.heading @heading
+          xm.tilt    @tilt
+          xm.roll    @roll
+        end
+        xm.Scale do
+          xm.x @x
+          xm.y @y
+          xm.z @z
+        end
+        @link.render(xm)
+        # ResourceMap needs to be implemented still
+      end
+    end
+
   end
 end
