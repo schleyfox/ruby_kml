a8f4059eca9e4416c4db5193c63d7213a767bf14
Ian Vaughan <github@ianvaughan.co.uk>
2012-05-08 18:27:11 +0100

    remove test helper methods
    `write_and_show` only saved the file
    `show_file` mostly commented out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,20 +4,8 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 require 'pp'
 require 'test/unit'
-
 require 'kml'
 
 class Test::Unit::TestCase
-  def write_and_show(kml, file)
-    File.open(file, 'w') { |f| f.write kml.render }
-    # show_file(file)
-  end
 
-  def show_file(file)
-    raise
-    #tests should probably not launch visual external applications
-    #cmd = "open -a /Applications/Google\\ Earth.app/ #{File.expand_path(file)}"
-    #puts "executing command: #{cmd}"
-    #`#{cmd}`
-  end
 end
